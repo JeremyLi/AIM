32271ea57da1df77f2fe111823b907ae68b42888
Jeremy Li <beexploring@gmail.com>
2014-11-09 21:10:10 +0800

    init words in graph db

---
+++ Rakefile
@@ -0,0 +1,2 @@
+load '/home/jermeyli/.rvm/gems/jruby-1.7.16.1@neo4j-3/gems/neo4j-core-3.0.7/lib/neo4j/tasks/neo4j_server.rake'
+#load 'neo4j/tasks/neo4j_server.rake'
---
+++ birth/construct_db_words.rb
@@ -0,0 +1,31 @@
+# coding: utf-8
+require 'neo4j-core'
+#require 'neo4j-embedded'
+#require 'neo4j-embedded/embedded_session'
+
+session = Neo4j::Session.open(:embedded_db, '../db/neo4j/development/data/graph.db')
+session.start
+node = Neo4j::Node.create({name: 'enter'})
+#node0 = Neo4j::Node.load 0
+puts "Loads node #{node0[:name]} with id #{node0.id}"
+  
+wordsFile = File.new('words1.txt')
+l = 0
+for line in wordsFile
+  words = line.split
+ l += 1
+  print l,  ': '
+  words.each_with_index do |w, i|
+    if i>0 && i%2 == 0
+      print w
+        node = Neo4j::Node.create({words:  w})
+      ids=[]
+       ids << node.id
+       node0[w] = ids
+    end
+  end
+  print "\n"
+end
+
+#session.stop
+p 'birthday'
